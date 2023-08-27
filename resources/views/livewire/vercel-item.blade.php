19fd561461d7abe7051b81ef8c51e5918bca6de6
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-08-27 19:27:43 +0200

    [ quickfix]

--- resources/views/livewire/vercel-item.blade.php
+++ resources/views/livewire/vercel-item.blade.php
@@ -1,7 +1,13 @@
 <div class="p-5 rounded-xl shadow-xl bg-zinc-300 my-3 mx-2 hover:bg-blue-100 transition-colors flex">
     <img class="float-left w-16 mr-3" src="">
     <div>
-        <h6 class="font-bold  text-base capitalize"><a target="_blank" href="https://{{ $site['targets']['production']['url']}}">{{ $site['name']}} </a></h6>
+        <h6 class="font-bold  text-base capitalize">
+            @if(isset($site['targets']['production']))
+                <a target="_blank" href="https://{{ $site['targets']['production']['url']}}">{{ $site['name']}} </a>
+            @else
+                {{ $site['name']}}
+            @endif
+        </h6>
         <p class="text-blue-800">
             <span class="font-bold text-black">Provider:</span> 
             @if($site['link']['type'] == 'github')
